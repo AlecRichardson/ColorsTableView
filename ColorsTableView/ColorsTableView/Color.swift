8dd314b29810ffc97735635ab6e3295cee878f75
Alec Richardson <alec.richardson@eventbrite.com>
2019-03-31 13:38:08 -0500

    Added detail view

---
+++ ColorsTableView/ColorsTableView/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ColorsTableView
+//
+//  Created by Alec Richardson on 3/31/19.
+//  Copyright © 2019 Alec Richardson. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
